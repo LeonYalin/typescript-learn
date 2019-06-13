9dde1385223d65b227ea9573ae7fc48c5b5129c4
Leon Yalin <leon.yalin@cellebrite.com>
2019-06-13 13:16:12 +0300

    decorators part 3

--- ts/decorators.ts
+++ ts/decorators.ts
@@ -14,22 +14,54 @@ function sealedWithParams(param: string) {
   }
 }
 
+function logger<TFunction extends Function>(target: TFunction): TFunction {
+  const newConstructor: Function = function() {
+    log('Creating new instance...', target);
+  }
+
+  newConstructor.prototype = Object.create(target.prototype);
+  newConstructor.prototype.constructor = target;
+  return <TFunction>newConstructor;
+}
+
+function readonly(target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+  log(`@readonly: Setting ${propertyKey} to be read-only`);
+  descriptor.writable = false;
+}
+
+function writable(isWritable: boolean) {
+  return function (target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+    log(`@writableSetting ${propertyKey} is now writealbe: ${isWritable}`);
+    descriptor.writable = isWritable;
+  }
+}
+
 @sealed
 @sealedWithParams('Hello kitty!')
+@logger
 class Cat {
   constructor(private name: string) {
   }
 
+  @readonly
   public sayMeaw(): string {
     return `${this.name}: Meaaaw!`;
+  }
+
+  @writable(false)
+  public toString() {
+    return `Cat[name=${this.name}]`;
   }
 }
 
 function decorators() {
   delimeterMsg('DECORATORS');
 
-  log('class decorator example:', new Cat('Tom')['sealed']);
-  log('class decorator example:', new Cat('Tom')['sealedWithParams']);
+  log('class decorator example: @sealed', new Cat('Tom')['sealed']);
+  log('class decorator with parameters example: @sealedWithParams("...")', new Cat('Tom')['sealedWithParams']);
+  log('class decorators with constructor replacement example: @logger');
+  log('method decorators example: @readonly');
+  log('method decorators with parameters example: @writable("...")');
 }
 
-export { sealed, decorators as default };
+export { sealed, sealedWithParams, logger, readonly, writable, decorators as default };
