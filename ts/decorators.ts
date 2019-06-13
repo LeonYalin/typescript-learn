d27fb73b80db3819c42dd3f7a90fa1b7c9ed16a8
Leon Yalin <yelinichevl@gmail.com>
2019-06-13 09:50:11 +0300

    decorators part 2

--- ts/decorators.ts
+++ ts/decorators.ts
@@ -1,5 +1,35 @@
 import delimeterMsg, { log } from "./utils";
 
-export default function decorators() {
-  delimeterMsg('DECORATORS');
+function sealed(constructor: Function): void {
+  log('Sealed decorator - sealing the class constructor');
+  // Object.seal(constructor);
+  // Object.seal(constructor.prototype);
+  constructor.prototype.sealed = '@sealed:true';
 }
+
+function sealedWithParams(param: string) {
+  return function(constructor: Function): void {
+    log('Sealed decorator with parameters');
+    constructor.prototype.sealedWithParams = `@sealedWithParams: ${param}`;
+  }
+}
+
+@sealed
+@sealedWithParams('Hello kitty!')
+class Cat {
+  constructor(private name: string) {
+  }
+
+  public sayMeaw(): string {
+    return `${this.name}: Meaaaw!`;
+  }
+}
+
+function decorators() {
+  delimeterMsg('DECORATORS');
+
+  log('class decorator example:', new Cat('Tom')['sealed']);
+  log('class decorator example:', new Cat('Tom')['sealedWithParams']);
+}
+
+export { sealed, decorators as default };
